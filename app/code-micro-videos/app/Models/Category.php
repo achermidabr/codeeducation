2af358cd1d83657babe302f8a6b8ce5bd74a7170
Alexandre Hermida <achermida@gmail.com>
2021-06-17 19:28:49 -0300

    Adicionando o incrementing

--- app/code-micro-videos/app/Models/Category.php
+++ app/code-micro-videos/app/Models/Category.php
@@ -9,13 +9,16 @@ class Category extends Model
 {
     protected $fillable = ['name','description','is_active'];
     protected $dates = ['deleted_at'];
-
+    protected $casts = [
+        'id' => 'string'
+    ];
     public static function boot(){
         parent::boot();
         static::creating(function($obj){
             $obj->id = Uuid::uuid4();
         });
     }
+    public $incrementing = false;
 }
 
 
--- app/code-micro-videos/app/Models/Genre.php
+++ app/code-micro-videos/app/Models/Genre.php
@@ -8,12 +8,14 @@ class Genre extends Model
 {
     protected $fillable = ['name','is_active'];
     protected $dates = ['deleted_at'];
-
+    protected $casts = [
+        'id' => 'string'
+    ];
     public static function boot(){
         parent::boot();
         static::creating(function($obj){
             $obj->id = Uuid::uuid4();
         });
     }
-
+    public $incrementing = false;
 }
